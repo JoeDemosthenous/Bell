32cd65587754afbe07164cc504d7a8656b22ac50
Chema Vazquez <cvazquez1@ies-eugeni.cat>
2024-02-16 18:11:59 +0100

    raro

---
+++ miasf.php
@@ -0,0 +1,121 @@
+<!-- ======= Portfolio Section ======= -->
+<section id="portfolio" class="portfolio">
+
+  <div class="container" data-aos="fade-up">
+
+    <div class="section-title">
+      <h2>Material</h2>
+      <p>Per jugar al pàdel necessites els següents materials:<br>
+        Pales de pàdel, bambes de pàdel, pilotes de pàdel, paletero, complements de pàdel.</p>
+    </div>
+
+    <ul id="portfolio-flters" class="d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
+      <li data-filter="*" class="filter-active">Tot</li>
+      <li data-filter=".filter-app">Pales</li>
+      <li data-filter=".filter-card">Pilotes</li>
+      <li data-filter=".filter-web">Bambes</li>
+      <li data-filter=".filter-pal">Paleteros</li>
+    </ul>
+
+    <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+        <div class="portfolio-img"><img src="img/PBC.jpeg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Pala Black Crown Piton</h4>
+          <p>Perfecte per fer els cops agressius</p>
+          <a href="img/PBC.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Pala Black Crown"><i class="bx bx-plus"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+        <div class="portfolio-img"><img src="img/BX.jpeg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Bambes Joma</h4>
+          <a href="img/BX.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Bambes Joma"><i class="bx bx-plus"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 portfolio-item filter-pal">
+        <div class="portfolio-img"><img src="img/portfolio/Paletero.jpeg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Paletero BULLPADEL</h4>
+          <p>La borsa Bullpadel Flow està <br>dissenyada perquè puguis portar l'equip<br> còmodament</p>
+          <a href="img/portfolio/Paletero.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Paletero Bullpadel"><i class="bx bx-plus"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+        <div class="portfolio-img"><img src="img/PBP.jpeg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Pala BULLPADEL</h4>
+          <p>És una pala ultralleugera, còmoda i molt manejable</p>
+          <a href="img/PBP.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Pala Bullpadel"><i class="bx bx-plus"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+        <div class="portfolio-img"><img src="img/PiS.jpeg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Pilotes SIUX</h4>
+          <a href="img/PiS.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Pilotes SIUX"><i class="bx bx-plus"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+        <div class="portfolio-img"><img src="img/BN.jpeg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Bambes NOX</h4>
+          <a href="img/BN.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Bambes NOX"><i class="bx bx-plus"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+        <div class="portfolio-img"><img src="img/PS.jpeg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Pala SIUX</h4>
+          <p>Materials que proporcionen una gran <br>resistència i durabilitat</p>
+          <a href="img/PS.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Pala SIUX"><i class="bx bx-plus"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 portfolio-item filter-pal">
+        <div class="portfolio-img"><img src="img/portfolio/Paletero2.jpeg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Paletero Black Crown</h4>
+          <p>Gran disseny amb un compartiment<br> principal amb cremallera </p>
+          <a href="img/portfolio/Paletero2.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Paletero Black Crown"><i class="bx bx-plus"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+        <div class="portfolio-img"><img src="img/PiBC.jpeg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Pilotes Black Crown</h4>
+          <a href="img/PiBC.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Pilotes Black Crown"><i class="bx bx-plus"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+        <div class="portfolio-img"><img src="img/PiBP.jpeg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Pilotes Bullpadel</h4>
+          <a href="img/PiBP.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Pilotes Bullpadel"><i class="bx bx-plus"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+        <div class="portfolio-img"><img src="img/BBP.jpeg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Bambes BULLPADEL</h4>
+          <a href="img/BBP.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Bambes BULLPADEL"><i class="bx bx-plus"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 portfolio-item filter-pal">
+        <div class="portfolio-img"><img src="img/portfolio/Paletero3.jpeg" class="img-fluid" alt=""></div>
+        <div class="portfolio-info">
+          <h4>Paletero SIUX</h4>
+          <p>El paleter perfecte per a jugadors que <br>busquen un disseny pràctic.</p>
+          <a href="img/portfolio/Paletero3.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Paletero SIUX"><i class="bx bx-plus"></i></a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section><!-- End Portfolio Section -->
--- portfolio.php
+++ portfolio.php
@@ -1,163 +1,171 @@
 <!-- ======= Portfolio Section ======= -->
 <section class="portfolio" id="portfolio">
+  <link href="assets/css/es.css" rel="stylesheet">
+  <div class="container text-center">
+    <h2>
+      Portfolio
+    </h2>
 
-<div class="container text-center">
-  <h2>
-    Portfolio
-  </h2>
+    <p>
+      Productes
+    </p>
+  </div>
+  <ul id="portfolio-flters" class="d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
+    <li data-filter="*" class="filter-active">Tot</li>
+    <li data-filter=".filter-app">Ratolins</li>
+    <li data-filter=".filter-card">Components</li>
+    <li data-filter=".filter-web">Monitors</li>
+    <li data-filter=".filter-pal">Cascs</li>
+    <li data-filter=".filter-tec">Teclats</li>
+  </ul>
 
-  <p>
-    Voluptua scripserit per ad, laudem viderer sit ex. Ex alia corrumpit voluptatibus usu, sed unum convenire id. Ut cum nisl moderatius, Per nihil dicant commodo an.
-  </p>
-</div>
+  <div class="portfolio-grid">
+    <div class="row">
+      <div class="col-lg-3 col-sm-6 col-xs-12 filter-pal">
+        <div class="card card-block">
+          <a href="assets/img/porf-1.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-1.jpg">
+            <div class="portfolio-over">
+              <div>
+                <h3 class="card-title">
+                  The Dude Rockin'
+                </h3>
 
-<div class="portfolio-grid">
-  <div class="row">
-    <div class="col-lg-3 col-sm-6 col-xs-12">
-      <div class="card card-block">
-        <a href="assets/img/porf-1.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-1.jpg">
-          <div class="portfolio-over">
-            <div>
-              <h3 class="card-title">
-                The Dude Rockin'
-              </h3>
-
-              <p class="card-text">
-                Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
-              </p>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
+                </p>
+              </div>
             </div>
-          </div>
-        </a>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-sm-6 col-xs-12">
+        <div class="card card-block">
+          <a href="assets/img/porf-2.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-2.jpg">
+            <div class="portfolio-over">
+              <div>
+                <h3 class="card-title">
+                  The Dude Rockin'
+                </h3>
+
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
+                </p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-sm-6 col-xs-12">
+        <div class="card card-block">
+          <a href="assets/img/porf-3.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-3.jpg">
+            <div class="portfolio-over">
+              <div>
+                <h3 class="card-title">
+                  The Dude Rockin'
+                </h3>
+
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
+                </p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-sm-6 col-xs-12">
+        <div class="card card-block">
+          <a href="assets/img/porf-4.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-4.jpg">
+            <div class="portfolio-over">
+              <div>
+                <h3 class="card-title">
+                  The Dude Rockin'
+                </h3>
+
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
+                </p>
+              </div>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
 
-    <div class="col-lg-3 col-sm-6 col-xs-12">
-      <div class="card card-block">
-        <a href="assets/img/porf-2.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-2.jpg">
-          <div class="portfolio-over">
-            <div>
-              <h3 class="card-title">
-                The Dude Rockin'
-              </h3>
+    <div class="row">
+      <div class="col-lg-3 col-sm-6 col-xs-12">
+        <div class="card card-block">
+          <a href="assets/img/porf-5.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-5.jpg">
+            <div class="portfolio-over">
+              <div>
+                <h3 class="card-title">
+                  The Dude Rockin'
+                </h3>
 
-              <p class="card-text">
-                Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
-              </p>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
+                </p>
+              </div>
             </div>
-          </div>
-        </a>
+          </a>
+        </div>
       </div>
-    </div>
 
-    <div class="col-lg-3 col-sm-6 col-xs-12">
-      <div class="card card-block">
-        <a href="assets/img/porf-3.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-3.jpg">
-          <div class="portfolio-over">
-            <div>
-              <h3 class="card-title">
-                The Dude Rockin'
-              </h3>
+      <div class="col-lg-3 col-sm-6 col-xs-12">
+        <div class="card card-block">
+          <a href="assets/img/porf-6.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-6.jpg">
+            <div class="portfolio-over">
+              <div>
+                <h3 class="card-title">
+                  The Dude Rockin'
+                </h3>
 
-              <p class="card-text">
-                Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
-              </p>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
+                </p>
+              </div>
             </div>
-          </div>
-        </a>
+          </a>
+        </div>
       </div>
-    </div>
 
-    <div class="col-lg-3 col-sm-6 col-xs-12">
-      <div class="card card-block">
-        <a href="assets/img/porf-4.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-4.jpg">
-          <div class="portfolio-over">
-            <div>
-              <h3 class="card-title">
-                The Dude Rockin'
-              </h3>
+      <div class="col-lg-3 col-sm-6 col-xs-12">
+        <div class="card card-block">
+          <a href="assets/img/porf-7.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-7.jpg">
+            <div class="portfolio-over">
+              <div>
+                <h3 class="card-title">
+                  The Dude Rockin'
+                </h3>
 
-              <p class="card-text">
-                Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
-              </p>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
+                </p>
+              </div>
             </div>
-          </div>
-        </a>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-sm-6 col-xs-12">
+        <div class="card card-block">
+          <a href="assets/img/porf-8.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-8.jpg">
+            <div class="portfolio-over">
+              <div>
+                <h3 class="card-title">
+                  The Dude Rockin'
+                </h3>
+
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
+                </p>
+              </div>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   </div>
-
-  <div class="row">
-    <div class="col-lg-3 col-sm-6 col-xs-12">
-      <div class="card card-block">
-        <a href="assets/img/porf-5.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-5.jpg">
-          <div class="portfolio-over">
-            <div>
-              <h3 class="card-title">
-                The Dude Rockin'
-              </h3>
-
-              <p class="card-text">
-                Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
-              </p>
-            </div>
-          </div>
-        </a>
-      </div>
-    </div>
-
-    <div class="col-lg-3 col-sm-6 col-xs-12">
-      <div class="card card-block">
-        <a href="assets/img/porf-6.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-6.jpg">
-          <div class="portfolio-over">
-            <div>
-              <h3 class="card-title">
-                The Dude Rockin'
-              </h3>
-
-              <p class="card-text">
-                Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
-              </p>
-            </div>
-          </div>
-        </a>
-      </div>
-    </div>
-
-    <div class="col-lg-3 col-sm-6 col-xs-12">
-      <div class="card card-block">
-        <a href="assets/img/porf-7.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-7.jpg">
-          <div class="portfolio-over">
-            <div>
-              <h3 class="card-title">
-                The Dude Rockin'
-              </h3>
-
-              <p class="card-text">
-                Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
-              </p>
-            </div>
-          </div>
-        </a>
-      </div>
-    </div>
-
-    <div class="col-lg-3 col-sm-6 col-xs-12">
-      <div class="card card-block">
-        <a href="assets/img/porf-8.jpg" class="portfolio-lightbox" data-gallery="portfolioGallery"><img alt="" src="assets/img/porf-8.jpg">
-          <div class="portfolio-over">
-            <div>
-              <h3 class="card-title">
-                The Dude Rockin'
-              </h3>
-
-              <p class="card-text">
-                Lorem ipsum dolor sit amet, eu sed suas eruditi honestatis.
-              </p>
-            </div>
-          </div>
-        </a>
-      </div>
-    </div>
-  </div>
-</div>
 </section><!-- End Portfolio Section -->
