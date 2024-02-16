4880ac073ab84c929062b51c510724ede53ccf25
JoeDemosthenous <jdemosthe@ies-eugeni.cat>
2024-02-16 19:34:07 +0100

    Canvis al about.php

--- about.php
+++ about.php
@@ -11,8 +11,9 @@
       <p>
         Aquesta pagina web es un projecte final en el que practiquem el us de differents tecnologies. Les tecnologies que estem fent servir en aquesta pagina web son les seguents:
         <br><b>PhP</b>:Estem fent servir PHP per a fer visibles els nostres productes ,tambe l'estem fent servir per a modificar els nostres productes mediant aquesta pagina web.
-        <br><b>Wordpress i Woocomerce:</b>Estem fent servir el Wordpress i l'extencio Woocomerce entre altres coses per a crear productes, etiquetes i altres coses.
-      </p>
+        <br><b>Wordpress i Woocommerce:</b>Estem fent servir el Wordpress i l'extencio Woocomerce entre altres coses per a crear productes, etiquetes i altres coses.
+        <br><b>Json de Woocommerce:</b>Hem fet servir l'API REST de WooCommerce per a accedir a les dades del wordpress sense modificar la base de dades però amb tot l'accés a Crear,Llistar,Actualitzar i Eliminar (CRUD).
+        </p>
     </div>
     </div>
     </div>
